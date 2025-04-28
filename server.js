d4ceab58f88050e70c5f417518ec533a1627a1fd
Mitchellvandevorst <42737779+Mitchellvandevorst@users.noreply.github.com>
2025-04-28 10:20:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ const PORT = process.env.PORT || 3000;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// allow CORS
 app.use(cors());
+
+// serve static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
 // fallback for all unknown routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// now listen AFTER all routes are defined
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
